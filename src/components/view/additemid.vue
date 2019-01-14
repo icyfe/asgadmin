674635371c08b63a8864abe2040ebfba11a384f2
cc <15805986937@163.com>
2019-01-14 09:20:34 +0800

    2019-1-14

--- src/components/view/additemid.vue
+++ src/components/view/additemid.vue
@@ -1,5 +1,8 @@
 <template>
-  <a-form @submit="handleSubmit" :autoFormCreate="(form)=>{this.form = form}">
+  <a-form
+    @submit="handleSubmit"
+    :autoFormCreate="(form)=>{this.form = form}"
+  >
     <a-form-item
       label="商品ID"
       :labelCol="{ span: 5 }"
@@ -7,7 +10,7 @@
       fieldDecoratorId="num_iid"
       :fieldDecoratorOptions="{rules: [{type:'string',pattern: /^[1-9]+$/, required: true, message: '请输入合法的seller_id!' }]}"
     >
-      <a-input id="num_iid"/>
+      <a-input id="num_iid" />
     </a-form-item>
     <a-form-item
       label="商品类别"
@@ -16,7 +19,10 @@
       fieldDecoratorId="gender"
       :fieldDecoratorOptions="{rules: [{ required: true, message: 'Please select your gender!' }]}"
     >
-      <a-select placeholder="请选择类别" id="gender">
+      <a-select
+        placeholder="请选择类别"
+        id="gender"
+      >
         <a-select-option value="male">男装</a-select-option>
         <a-select-option value="female">女装</a-select-option>
       </a-select>
@@ -28,7 +34,7 @@
       fieldDecoratorId="zk_final_price"
       :fieldDecoratorOptions="{rules: [{type:'string',pattern: /^[1-9]+$/, required: true, message: '请输入数字!' }]}"
     >
-      <a-input id="zk_final_price"/>
+      <a-input id="zk_final_price" />
     </a-form-item>
     <a-form-item
       label="库存数"
@@ -37,7 +43,7 @@
       fieldDecoratorId="volume"
       :fieldDecoratorOptions="{rules: [{type:'string',pattern: /^[1-9]+$/, required: true, message: '请输入数字!' }]}"
     >
-      <a-input id="volume"/>
+      <a-input id="volume" />
     </a-form-item>
     <a-form-item
       label="商品主图"
@@ -48,23 +54,64 @@
     >
       <a-upload
         label="商品主图"
-        action="//jsonplaceholder.typicode.com/posts/"
+        action="//localhost:3302/api/post/upload"
         listType="picture-card"
-        :fileList="fileList"
         @preview="handlePreview"
         @change="handleChange"
       >
         <div v-if="fileList.length < 5">
-          <a-icon type="plus"/>
+          <a-icon type="plus" />
           <div class="ant-upload-text">上传图片</div>
         </div>
       </a-upload>
-      <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
-        <img alt="example" style="width: 100%" :src="previewImage">
+      <a-modal
+        :visible="previewVisible"
+        :footer="null"
+        @cancel="handleCancel"
+      >
+        <img
+          alt="example"
+          style="width: 100%"
+          :src="previewImage"
+        >
+      </a-modal>
+    </a-form-item>
+    <a-form-item
+      label="商品小图"
+      :labelCol="{ span: 5 }"
+      :wrapperCol="{ span: 12 }"
+      fieldDecoratorId="pict_url1"
+      :fieldDecoratorOptions="{rules: []}"
+    >
+      <a-upload
+        label="商品主图"
+        action="//localhost:3302/api/post/upload"
+        listType="picture-card"
+        @preview="handlePreview1"
+        @change="handleChange1"
+      >
+        <div v-if="fileList1.length < 5">
+          <a-icon type="plus" />
+          <div class="ant-upload-text">上传图片</div>
+        </div>
+      </a-upload>
+      <a-modal
+        :visible="previewVisible1"
+        :footer="null"
+        @cancel="handleCancel"
+      >
+        <img
+          alt="example"
+          style="width: 100%"
+          :src="previewImage1"
+        >
       </a-modal>
     </a-form-item>
     <a-form-item :wrapperCol="{ span: 12, offset: 5 }">
-      <a-button type="primary" htmlType="submit">新增</a-button>
+      <a-button
+        type="primary"
+        htmlType="submit"
+      >新增</a-button>
     </a-form-item>
   </a-form>
 </template>
@@ -75,7 +122,10 @@ export default {
     return {
       previewVisible: false,
       previewImage: "",
-      fileList: [{}]
+      fileList: [{}],
+      previewVisible1: false,
+      previewImage1: "",
+      fileList1: [{}]
     };
   },
   methods: {
@@ -87,8 +137,17 @@ export default {
       this.previewVisible = true;
       //onsole.log("file", file);
     },
+    handlePreview1(file) {
+      this.previewImage1 = file.url || file.thumbUrl;
+      this.previewVisible1 = true;
+      //onsole.log("file", file);
+    },
     handleChange({ fileList }) {
       this.fileList = fileList;
+      //console.log("fileList", fileList);
+    },
+    handleChange1({ fileList }) {
+      this.fileList1 = fileList;
       //console.log("fileList", fileList);
     },
     handleSubmit(e) {
